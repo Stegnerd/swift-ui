18d174c639a37475befc0639603b9d075103062c
Dalton Stegner <dmstegner@gmail.com>
2020-09-27 22:24:23 -0400

    1. Added custom map view to the content view
    1. Added custom map view to the content view

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,17 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-        }.padding()
+        VStack {
+            MapView()
+                // allows to bleed into top beside camera bump
+                .edgesIgnoringSafeArea(.top)
+                // when you specify only the height it automatically does the width
+                .frame(height:300)
+            
+            // To layer image on top of something
+            // give the image a negative offset vertically
+            // and padding on the bottom
+            CircleImage()
+                // (pulls the image up out of its boundaries)
+                .offset(y: -130)
+                // (pulls what ever is below into boundaries)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }.padding()
+            // this pushed everything to the top
+            Spacer()
+        }
     }
 }
 
