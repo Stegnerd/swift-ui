2615deab60eb8909eadf9a4fcfaa6678c34ff78c
Dalton Stegner <dmstegner@gmail.com>
2020-09-30 21:12:33 -0400

    1. Finished chapter 3. Added favoriting.

--- Landmarks/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/LandmarkDetail.swift
@@ -34,8 +34,22 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                    }){
+                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -53,6 +67,6 @@ struct LandmarkDetail: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarkData[0])
+        LandmarkDetail(landmark: landmarkData[0]).environmentObject(UserData())
     }
 }
